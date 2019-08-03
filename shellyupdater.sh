fb93fb5741dca9fc6651893c7bb1849c650528bf
florie1706 <florian.dester@web.de>
2019-08-03 20:19:37 +0200

    Update shellyupdater.sh

--- shellyupdater.sh
+++ shellyupdater.sh
@@ -6,7 +6,7 @@
 #  to be installed in advance:
 #  - avahi-utils
 #  - jq
-#  - webserver (which is running to server zip file for the firmware update
+#  - webserver (which is running to serve zip file for the firmware update
 #
 #  Please set these variables first:
 #  - WWWDIR where the firmware update has to be stored
