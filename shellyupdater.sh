818c95e64b4b1655114cef1dfcf0b5cf1b7b4997
florie1706 <26010442+florie1706@users.noreply.github.com>
2021-02-23 22:09:39 +0100

    Fix incorrect firmware version check

--- shellyupdater.sh
+++ shellyupdater.sh
@@ -14,7 +14,7 @@
 #  - USER shelly user, should be the same on every device within your network
 #  - PW password, should be the same on every device within your network
 #
-#  Florie1706, 2020
+#  Florie1706, 2021
 #
 ########################################################################################
 #
@@ -58,113 +58,122 @@ TRUNK=BETA
 
 #### check for correct TRUNK version ####
 if [ $TRUNK != "BETA" ] && [ $TRUNK != "PRE" ] ; then
-        TRUNK=STABLE
+	TRUNK=STABLE
 fi
 
 echo "${yellow}$TRUNK-Kanal wurde ausgewählt.${reset}"
 ECHOTRUNK=0
 
 #### check for availible shellies in your network ####
-for SHELLYIP in $(avahi-browse -d local -k -v -t -r -p _http._tcp | grep helly | grep 192 | cut -d ';' -f8 | sort -n)
-        do
-        UPDATE=0
-        OLDFIRMWAREAUTHTEST=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | grep 'Unauthorized' | cut -d ' ' -f2)
-        if [ -z $OLDFIRMWAREAUTHTEST ] ; then
-                SHELLYTYPE=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | jq .device.type | cut -d '"' -f2)
-                OLDFIRMWAREFULL=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | jq .fw | cut -d '"' -f2)
-                OLDFIRMWARE=$(echo $OLDFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
-        else
-                OLDFIRMWARE=""
-        fi
-        STABLENEWFIRMWAREFULL=$(curl -s https://api.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
+for SHELLYIP in $(avahi-browse -d local -k -v -t -r -p _http._tcp | grep helly | grep 192 | cut -d ';' -f8 | awk '{ print length(), $0 | "sort -n" }' | cut -d ' ' -f2)
+	do
+	UPDATE=0
+	OLDFIRMWAREAUTHTEST=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | grep 'Unauthorized' | cut -d ' ' -f2)
+	if [ -z $OLDFIRMWAREAUTHTEST ] ; then
+		SHELLYTYPE=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | jq .device.type | cut -d '"' -f2)
+		SHELLYNAME=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | jq .name)
+		OLDFIRMWAREFULL=$(curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/settings | jq .fw | cut -d '"' -f2)
+		OLDFIRMWARE=$(echo $OLDFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
+	else
+		OLDFIRMWARE=""
+	fi
+	STABLENEWFIRMWAREFULL=$(curl -s https://api.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
         if [ $TRUNK = "STABLE" ] ; then
                 NEWFIRMWAREFULL=$STABLENEWFIRMWAREFULL
         elif [ $TRUNK = "PRE" ] ; then
-                PRENEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
-                NEWFIRMWAREFULL=$PRENEWFIRMWAREFULL
-        else
-                PRENEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
-                BETANEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].beta_ver' | cut -d '"' -f2)
-                NEWFIRMWAREFULL=$BETANEWFIRMWAREFULL
-        fi
-        NEWFIRMWARE=$(echo $NEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
-        NEWFIRMWARECHECKSUM=$(echo $NEWFIRMWAREFULL | cut -d '@' -f2)
+		PRENEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
+		NEWFIRMWAREFULL=$PRENEWFIRMWAREFULL
+	else
+		PRENEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].version' | cut -d '"' -f2)
+		BETANEWFIRMWAREFULL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].beta_ver' | cut -d '"' -f2)
+		NEWFIRMWAREFULL=$BETANEWFIRMWAREFULL
+	fi
+	NEWFIRMWARE=$(echo $NEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
+	NEWFIRMWARECHECKSUM=$(echo $NEWFIRMWAREFULL | cut -d '@' -f2)
 #### if no firmware version or shelly type is determinated an error message will be shown ####
-        if [ -n "$OLDFIRMWARE" ] && [ -n "$SHELLYTYPE" ] ; then
+	if [ -n "$OLDFIRMWARE" ] && [ -n "$SHELLYTYPE" ] ; then
 #### check for newer firmware availible ####
-                OLDFIRMWAREPART1=$(echo $OLDFIRMWARE | cut -d '.' -f1)
-                OLDFIRMWAREPART2=$(echo $OLDFIRMWARE | cut -d '.' -f2)
-                OLDFIRMWAREPART3=$(echo $OLDFIRMWARE | cut -d '.' -f3)
-                OLDFIRMWARESUM=$(((OLDFIRMWAREPART1 * 10000) + (OLDFIRMWAREPART2 * 100) + OLDFIRMWAREPART3))
-                OLDFIRMWARECHECKSUM=$(echo $OLDFIRMWAREFULL | cut -d '@' -f2)
-                OLDFIRMWAREDATETIME=$(echo $OLDFIRMWAREFULL | cut -d '/' -f1)
+		OLDFIRMWAREPART1=$(echo $OLDFIRMWARE | cut -d '.' -f1)
+		OLDFIRMWAREPART2=$(echo $OLDFIRMWARE | cut -d '.' -f2)
+		OLDFIRMWAREPART3=$(echo $OLDFIRMWARE | cut -d '.' -f3)
+		OLDFIRMWARESUM=$(((OLDFIRMWAREPART1 * 10000) + (OLDFIRMWAREPART2 * 100) + OLDFIRMWAREPART3))
+       	        OLDFIRMWARECHECKSUM=$(echo $OLDFIRMWAREFULL | cut -d '@' -f2)
+		OLDFIRMWAREDATETIME=$(echo $OLDFIRMWAREFULL | cut -d '/' -f1)
                 NEWFIRMWAREPART1=$(echo $NEWFIRMWARE | cut -d '.' -f1)
                 NEWFIRMWAREPART2=$(echo $NEWFIRMWARE | cut -d '.' -f2)
                 NEWFIRMWAREPART3=$(echo $NEWFIRMWARE | cut -d '.' -f3)
                 NEWFIRMWARESUM=$(((NEWFIRMWAREPART1 * 10000) + (NEWFIRMWAREPART2 * 100) + NEWFIRMWAREPART3))
                 NEWFIRMWARECHECKSUM=$(echo $NEWFIRMWAREFULL | cut -d '@' -f2)
-                NEWFIRMWAREDATETIME=$(echo $NEWFIRMWAREFULL | cut -d '/' -f1)
+		NEWFIRMWAREDATETIME=$(echo $NEWFIRMWAREFULL | cut -d '/' -f1)
 #### check if newer version is availible and if there is a newer version then your choosen trunk on STABLE/PRE when PRE/BETA was selected) for new shipped devices the current firmware will be installed ####
-                if [ $NEWFIRMWARESUM -ge $OLDFIRMWARESUM ] && ([ $NEWFIRMWARECHECKSUM != $OLDFIRMWARECHECKSUM ] || [ $NEWFIRMWAREDATETIME != $OLDFIRMWAREDATETIME ]) || ([ $PRENEWFIRMWAREFULL != $FIRMWAREFULL ] || [ $STABLENEWFIRMWAREFULL != $FIRMWAREFULL ]); then
-                        UPDATE=$TRUNK
-                        if [ $TRUNK != "STABLE" ] ; then
-                                STABLENEWFIRMWARE=$(echo $STABLENEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
-                                STABLENEWFIRMWAREPART1=$(echo $STABLENEWFIRMWARE | cut -d '.' -f1)
-                                STABLENEWFIRMWAREPART2=$(echo $STABLENEWFIRMWARE | cut -d '.' -f2)
-                                STABLENEWFIRMWAREPART3=$(echo $STABLENEWFIRMWARE | cut -d '.' -f3)
-                                STABLENEWFIRMWARESUM=$(((STABLENEWFIRMWAREPART1 * 10000) + (STABLENEWFIRMWAREPART2 * 100) + STABLENEWFIRMWAREPART3))
-                                STABLENEWFIRMWAREDATETIME=$(echo $STABLENEWFIRMWAREFULL | cut -d '/' -f1)
-                                if [ $STABLENEWFIRMWARESUM -ge $NEWFIRMWARESUM ] && [ $STABLENEWFIRMWAREDATETIME != $NEWFIRMWAREDATETIME ] ; then
-                                        UPDATE=STABLE
-                                        NEWFIRMWARE=$STABLENEWFIRMWARE
-                                fi
-                        fi
-                        if [ $TRUNK != "PRE" ] ; then
-                                PRENEWFIRMWARE=$(echo $PRENEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
-                                PRENEWFIRMWAREPART1=$(echo $PRENEWFIRMWARE | cut -d '.' -f1)
+		if [ $NEWFIRMWARESUM -ge $OLDFIRMWARESUM ] && { [ $NEWFIRMWARECHECKSUM != $OLDFIRMWARECHECKSUM ] || [ $NEWFIRMWAREDATETIME != $OLDFIRMWAREDATETIME ] || [ $PRENEWFIRMWAREFULL != $OLDFIRMWAREFULL ] || [ $STABLENEWFIRMWAREFULL != $OLDFIRMWAREFULL ]; }; then
+			UPDATE=$TRUNK
+			if [ $TRUNK != "STABLE" ] ; then
+	        	        STABLENEWFIRMWARE=$(echo $STABLENEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
+               			STABLENEWFIRMWAREPART1=$(echo $STABLENEWFIRMWARE | cut -d '.' -f1)
+		                STABLENEWFIRMWAREPART2=$(echo $STABLENEWFIRMWARE | cut -d '.' -f2)
+       	        		STABLENEWFIRMWAREPART3=$(echo $STABLENEWFIRMWARE | cut -d '.' -f3)
+		                STABLENEWFIRMWARESUM=$(((STABLENEWFIRMWAREPART1 * 10000) + (STABLENEWFIRMWAREPART2 * 100) + STABLENEWFIRMWAREPART3))
+				STABLENEWFIRMWAREDATETIME=$(echo $STABLENEWFIRMWAREFULL | cut -d '/' -f1)
+	        	        if [ $STABLENEWFIRMWARESUM -ge $NEWFIRMWARESUM ] && [ $STABLENEWFIRMWAREDATETIME != $NEWFIRMWAREDATETIME ] ; then
+					UPDATE=STABLE
+					NEWFIRMWARE=$STABLENEWFIRMWARE
+				fi
+			fi
+			if [ $TRUNK != "PRE" ] ; then
+                       	        PRENEWFIRMWARE=$(echo $PRENEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f1)
+                               	PRENEWFIRMWAREPART1=$(echo $PRENEWFIRMWARE | cut -d '.' -f1)
                                 PRENEWFIRMWAREPART2=$(echo $PRENEWFIRMWARE | cut -d '.' -f2)
-                                PRENEWFIRMWAREPART3=$(echo $PRENEWFIRMWARE | cut -d '.' -f3)
-                                PRENEWFIRMWARESUM=$(((PRENEWFIRMWAREPART1 * 10000) + (PRENEWFIRMWAREPART2 * 100) + PRENEWFIRMWAREPART3))
-                                PRENEWFIRMWAREDATETIME=$(echo $PRENEWFIRMWAREFULL | cut -d '/' -f1)
-                                if [ $PRENEWFIRMWARESUM -ge $NEWFIRMWARESUM ] && [ $PRENEWFIRMWAREDATETIME != $NEWFIRMWAREDATETIME ] ; then
-                                        UPDATE=PRE
-                                        NEWFIRMWARE=$PRENEWFIRMWARE
+       	                        PRENEWFIRMWAREPART3=$(echo $PRENEWFIRMWARE | cut -d '.' -f3)
+               	                PRENEWFIRMWARESUM=$(((PRENEWFIRMWAREPART1 * 10000) + (PRENEWFIRMWAREPART2 * 100) + PRENEWFIRMWAREPART3))
+				PRENEWFIRMWAREDATETIME=$(echo $PRENEWFIRMWAREFULL | cut -d '/' -f1)
+                       	        if [ $PRENEWFIRMWARESUM -ge $NEWFIRMWARESUM ] && [ $PRENEWFIRMWAREDATETIME != $NEWFIRMWAREDATETIME ] ; then
+                               	        UPDATE=PRE
+					NEWFIRMWARE=$PRENEWFIRMWARE
                                 fi
-                        fi
-                fi
-                if ([ $UPDATE != $TRUNK ] && ([ $UPDATE = "PRE" ] && [ $PRENEWFIRMWAREDATETIME = $OLDFIRMWAREDATETIME ])) || ([ $UPDATE = "BETA" ] && [ $NEWFIRMWAREDATETIME = $OLDFIRMWAREDATETIME ]) ; then
-                        UPDATE=0
-                fi
-                if [ $UPDATE != 0 ] ; then
-                        if [ $ECHOTRUNK -eq 0 ] && [ $UPDATE != $TRUNK ]; then
-                                echo "${yellow}$UPDATE-Kanal hat neuere Firmware, $UPDATE wird nun verwendet.${reset}"
-                                ECHOTRUNK=1
-                        fi
-                        echo "$SHELLYIP ($SHELLYTYPE) ${yellow}Update von v$OLDFIRMWARE auf Version ${cyan}v$NEWFIRMWARE ${yellow}vorhanden${reset}."
-                        if [ $UPDATE = "BETA" ] ; then
-                                NEWFIRMWAREURL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].beta_url' | cut -d '"' -f2)
-                                NEWFIRMWAREFULL=$BETANEWFIRMWAREFULL
-                        elif [ $UPDATE = "PRE" ] ; then
-                                NEWFIRMWAREURL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].url' | cut -d '"' -f2)
-                                NEWFIRMWAREFULL=$PRENEWFIRMWAREFULL
-                        else
-                                NEWFIRMWAREURL=$(curl -s https://api.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].url' | cut -d '"' -f2)
-                        fi
-                        NEWFIRMWAREZIP=$(echo $NEWFIRMWAREFULL | cut -d '/' -f2)
+			fi
+		fi
+		if ([ $UPDATE != $TRUNK ] && ([ $UPDATE = "PRE" ] && [ $PRENEWFIRMWAREDATETIME = $OLDFIRMWAREDATETIME ])) || ([ $UPDATE = "BETA" ] && [ $NEWFIRMWAREDATETIME = $OLDFIRMWAREDATETIME ]) ; then
+			UPDATE=0
+		fi
+		OLDFIRMWARERC=$(echo $OLDFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f2)
+		if [ $OLDFIRMWARERC != $OLDFIRMWARE ] ; then
+			OLDFIRMWARE=$(echo $OLDFIRMWARE-$OLDFIRMWARERC)
+		fi
+		if [ $UPDATE != 0 ] ; then
+			if [ $ECHOTRUNK -eq 0 ] && [ $UPDATE != $TRUNK ]; then
+				echo "${yellow}$UPDATE-Kanal hat neuere Firmware, $UPDATE wird nun verwendet.${reset}"
+				ECHOTRUNK=1
+			fi
+			NEWFIRMWARERC=$(echo $NEWFIRMWAREFULL | cut -d 'v' -f2 | cut -d '@' -f1 | cut -d '-' -f2)
+			if [ $NEWFIRMWARERC != $NEWFIRMWARE ] ; then
+				NEWFIRMWARE=$(echo $NEWFIRMWARE-$NEWFIRMWARERC)
+			fi
+			echo "$SHELLYIP ($SHELLYTYPE) ${yellow}Update von v$OLDFIRMWARE auf Version ${cyan}v$NEWFIRMWARE ${yellow}vorhanden${reset}."
+			if [ $UPDATE = "BETA" ] ; then
+				NEWFIRMWAREURL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].beta_url' | cut -d '"' -f2)
+				NEWFIRMWAREFULL=$BETANEWFIRMWAREFULL
+			elif [ $UPDATE = "PRE" ] ; then
+				NEWFIRMWAREURL=$(curl -s https://repo.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].url' | cut -d '"' -f2)
+				NEWFIRMWAREFULL=$PRENEWFIRMWAREFULL
+			else
+				NEWFIRMWAREURL=$(curl -s https://api.shelly.cloud/files/firmware | jq '.data["'$SHELLYTYPE'"].url' | cut -d '"' -f2)
+			fi
+			NEWFIRMWAREZIP=$(echo $NEWFIRMWAREFULL | cut -d '/' -f2)
 #### download firmware to server or skip if it is already there ####
-                        if [ -f $WWWDIR/$SHELLYTYPE-$NEWFIRMWAREZIP.zip ] ; then
-                                echo "Firmware-Datei bereits vorhanden, überspringe Download"
-                        else
-                                echo "Lade Firmware-Datei (v$NEWFIRMWARE) für $SHELLYTYPE herunter."
-                                curl -s $NEWFIRMWAREURL --output $WWWDIR/$SHELLYTYPE-$NEWFIRMWAREZIP.zip
-                        fi
+			if [ -f $WWWDIR/$SHELLYTYPE-$NEWFIRMWAREZIP.zip ] ; then
+				echo "Firmware-Datei bereits vorhanden, überspringe Download"
+			else
+				echo "Lade Firmware-Datei (v$NEWFIRMWARE) für $SHELLYTYPE herunter."
+				curl -s $NEWFIRMWAREURL --output $WWWDIR/$SHELLYTYPE-$NEWFIRMWAREZIP.zip
+			fi
 #### send firmware update to shelly ####
-                        echo "Starte Firmware-Update bei $SHELLYIP ($SHELLYTYPE) "
-                        curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/ota?url=$WWWURL/$SHELLYTYPE-$NEWFIRMWAREZIP.zip > /dev/null 2>&1
-                else
-                        echo "$SHELLYIP ($SHELLYTYPE) ist ${green}up-to-date${reset} (v$OLDFIRMWARE)."
-                fi
-        else
-                echo "${red}Für $SHELLYIP konnte keine Firmware-Information abgerufen werden.${reset}"
-        fi
+			echo "Starte Firmware-Update bei $SHELLYIP ($SHELLYTYPE) "
+			curl -s --header "Authorization: Basic $AUTH" http://$SHELLYIP/ota?url=$WWWURL/$SHELLYTYPE-$NEWFIRMWAREZIP.zip > /dev/null 2>&1
+		else
+			echo "$SHELLYIP - $SHELLYNAME ($SHELLYTYPE) ist ${green}up-to-date${reset} (v$OLDFIRMWARE)."
+		fi
+	else
+		echo "${red}Für $SHELLYIP konnte keine Firmware-Information abgerufen werden.${reset}"
+	fi
 done
